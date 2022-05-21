53f1baf4cda4af9832a997c35e8530027184c18e
martinmcy <104663090+martinmcy@users.noreply.github.com>
2022-05-21 11:34:40 +0800

    1

--- main.ts
+++ main.ts
@@ -1,1 +1,87 @@
-
+function right () {
+    sensors.DDMmotor(
+    AnalogPin.P13,
+    0,
+    AnalogPin.P0,
+    100
+    )
+    sensors.DDMmotor(
+    AnalogPin.P15,
+    0,
+    AnalogPin.P0,
+    0
+    )
+}
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        front()
+    } else if (receivedNumber == 2) {
+        back()
+    } else if (receivedNumber == 3) {
+        left()
+    } else if (receivedNumber == 4) {
+        right()
+    } else {
+        stop()
+    }
+})
+function back () {
+    sensors.DDMmotor(
+    AnalogPin.P13,
+    1,
+    AnalogPin.P0,
+    100
+    )
+    sensors.DDMmotor(
+    AnalogPin.P15,
+    1,
+    AnalogPin.P0,
+    100
+    )
+}
+function front () {
+    sensors.DDMmotor(
+    AnalogPin.P13,
+    0,
+    AnalogPin.P0,
+    100
+    )
+    sensors.DDMmotor(
+    AnalogPin.P15,
+    0,
+    AnalogPin.P0,
+    100
+    )
+}
+function left () {
+    sensors.DDMmotor(
+    AnalogPin.P13,
+    0,
+    AnalogPin.P0,
+    0
+    )
+    sensors.DDMmotor(
+    AnalogPin.P15,
+    0,
+    AnalogPin.P0,
+    100
+    )
+}
+function stop () {
+    sensors.DDMmotor(
+    AnalogPin.P13,
+    0,
+    AnalogPin.P0,
+    0
+    )
+    sensors.DDMmotor(
+    AnalogPin.P15,
+    0,
+    AnalogPin.P0,
+    0
+    )
+}
+radio.setGroup(7)
+basic.forever(function () {
+	
+})
